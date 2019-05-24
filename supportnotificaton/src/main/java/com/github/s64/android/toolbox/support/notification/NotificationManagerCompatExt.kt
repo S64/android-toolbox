55faa80223ad366720b60e18d3400998d7581c4c
Shuma Yoshioka <s64.stdio@gmail.com>
2019-05-24 18:00:55 +0900

    Change package name

--- supportnotificaton/src/main/java/com/github/s64/android/toolbox/support/notification/NotificationManagerCompatExt.kt
+++ supportnotificaton/src/main/java/com/github/s64/android/toolbox/support/notification/NotificationManagerCompatExt.kt
@@ -1,4 +1,4 @@
-package jp.s64.android.toolbox.support.notification
+package com.github.s64.android.toolbox.support.notification
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
