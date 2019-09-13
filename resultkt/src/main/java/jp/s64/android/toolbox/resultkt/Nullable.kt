3cbc8e18fdc5678cd4b133bf40801a1d50ab4e55
Shuma Yoshioka <s64.stdio@gmail.com>
2019-09-13 16:19:42 +0900

    Create nullable to result

--- resultkt/src/main/java/jp/s64/android/toolbox/resultkt/Nullable.kt
+++ resultkt/src/main/java/jp/s64/android/toolbox/resultkt/Nullable.kt
@@ -2,15 +2,21 @@ package jp.s64.android.toolbox.resultkt
 
 sealed class Nullable<V> {
     abstract fun unwrap(): V?
+    abstract fun toResult(): Result<V>
 }
 
 data class NonNull<V>(val value: V): Nullable<V>() {
+
+    override fun toResult(): Result<V> = Success(value)
 
     override fun unwrap(): V? = value
 
 }
 
 data class Null<V>(val noValue: Unit = Unit): Nullable<V>() {
+
+    override fun toResult(): Result<V> = toResult(NullPointerException())
+    fun toResult(reason: Throwable): Result<V> = Failure(reason)
 
     override fun unwrap(): V? = null
 
